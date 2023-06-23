36dc7c8ce5515e0c7075b16fe8e00738371ef167
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-06-23 20:25:53 +0300

    finish 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,20 +25,20 @@ void swap_ints(int *a, int *b)
 void selection_sort(int *array, size_t size)
 {
 	int *min;
-	size_t n, m;
+	size_t x, y;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (n = 0; n < size - 1; n++)
+	for (x = 0; x < size - 1; x++)
 	{
-		min = array + n;
-		for (m = n + 1; m < size; m++)
-			min = (array[m] < *min) ? (array + m) : min;
+		min = array + x;
+		for (y = x + 1; y < size; y++)
+			min = (array[y] < *min) ? (array + y) : min;
 
-		if ((array + n) != min)
+		if ((array + x) != min)
 		{
-			swap_ints(array + n, min);
+			swap_ints(array + x, min);
 			print_array(array, size);
 		}
 	}
